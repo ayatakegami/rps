7b33729752c26571df30fc45f0b852e82c306c7d
ayatakegami <89535361+ayatakegami@users.noreply.github.com>
2021-09-21 12:12:33 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 # game.py
 import random
-
+print("Welcome to my rock paper scissors game!")
 
 print("Rock, Paper, Scissors, Shoot!")
 user_choice=input("choose 'paper' or 'rock' or 'scissors'")
@@ -12,3 +12,26 @@ computer_choice=random.choice(options)
 print("Computer chose:")
 print(computer_choice)
 
+if computer_choice == "rock" and user_choice=="scissors":
+    print("Computer wins!!")
+if computer_choice == "rock" and user_choice=="paper":
+    print("User wins!!")
+if computer_choice == "rock" and user_choice=="rock":
+    print("It's a tie, try again")
+
+if computer_choice=="paper" and user_choice=="scissors":
+    print("User wins!!")
+if computer_choice=="paper" and user_choice=="rock":
+    print("Computer wins!!")
+if computer_choice == "paper" and user_choice=="paper":
+  print("It's a tie, try again")
+
+if computer_choice == "scissors" and user_choice=="scissors":
+    print("It's a tie, try again")
+if computer_choice == "scissors" and user_choice=="rock":
+    print("User wins!!")
+if computer_choice == "scissors" and user_choice=="paper":
+    print("Computer wins!!")
+
+  
+print ("Thanks for playing!")
